2cb52e9088b1da8e161375dffcab6c20fc60b1c4
Erik Langendijk <erik.langendijk@gmail.com>
2022-04-29 14:59:15 +0200

    working DbConnection to Docker MSSQL container

---
+++ BotenODK-API/BotenODK-API/Data/BotenODK_APIContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BotenODK_API.Models;
+
+namespace BotenODK_API.Data
+{
+    public class BotenODK_APIContext : DbContext
+    {
+        public BotenODK_APIContext (DbContextOptions<BotenODK_APIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BotenODK_API.Models.User> User { get; set; }
+    }
+}
