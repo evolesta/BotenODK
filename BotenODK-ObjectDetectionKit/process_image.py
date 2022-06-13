9eb6c7bc3912f7878319a76ff3e0019979d0a3e4
Erik Langendijk <erik.langendijk@gmail.com>
2022-06-13 17:04:25 +0200

    first concept of processing a image script

--- BotenODK-ObjectDetectionKit/process_image.py
+++ BotenODK-ObjectDetectionKit/process_image.py
@@ -1,8 +1,8 @@
-from distutils.command.config import config
 from urllib.error import HTTPError
 import cv2
 import numpy as np
 import requests
+import os
 import datetime
 from cfg_authenticate import APIAuthenticate as auth
 
@@ -43,6 +43,82 @@ def GetFramesFromStream():
                 except HTTPError as err:
                     raise("HTTP error: " + err)
 
+def ProcessImage(filepath, id):
+    # change queue item into InProgress
+    try:
+        requests.post(auth.config["APIURL"] + "/FeedQueues/NextPhase/" + id, headers=headers, verify=False)
+    except HTTPError as err:
+        print("HTTP Error: " + err)
+        exit()
+
+    # Load Yolo
+    net = cv2.dnn.readNet(auth.config["YOLO"]["WEIGHTS"], auth.config["YOLO"]["CFG"])
+    classes = []
+    with open(auth.config["YOLO"]["NAMES"], "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    # Loading image
+    img = cv2.imread(filepath)
+    #img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            # this section will handle the detected objects - generating boxes and labels
+            label = str(classes[class_ids[i]])
+            x, y, w, h = boxes[i]
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 2, color, 3)
+
+            # add data to API
+
+    # write processed image with boxes to output directory
+    newFilepath = filepath.replace(".jpg", "-processed.jpg")
+    cv2.imwrite(newFilepath, img)
+
+    # set queue item to successful
+    try:
+        requests.post(auth.config["APIURL"] + "/FeedQueues/NextPhase/" + id, headers=headers, verify=False)
+    except HTTPError as err:
+        print("Error: " + err)
+
+    # delete original frame
+    os.remove(filepath)
+
 # get a list of new queue items to process
 accesstoken = auth.GetAccessToken(auth)
 headers = {
@@ -51,16 +127,17 @@ headers = {
 
 # get a list of new queue items from API to process
 try:
-    response = requests.get(auth.config["APIURL"] + "/FeedQueues/GetNewQueueItems?limit=" + auth.config["PROCESS_LIMIT"], headers=headers)
+    response = requests.get(auth.config["APIURL"] + "/FeedQueues/GetNewQueueItems?limit=" + str(auth.config["PROCESS_LIMIT"]), headers=headers, verify=False)
 except HTTPError as err:
     raise("HTTP Error: " + err)
 
 # create a json object to handle and loop trough results
 queueItems = response.json()
 for queueItem in queueItems:
-    print("Process queueitem " + queueItem["id"])
+    print("Process queueitem " + str(queueItem["id"]))
 
-    
+    # procces image
+    ProcessImage(queueItem["filepath"], str(queueItem["id"]))
 
 # get frames from livefeeds to analyse at the next run
 GetFramesFromStream()
